58fd318f7c3757c3318347ba0bc1dd255b64e294
chung <chung1905@gmail.com>
2018-11-01 18:12:12 +0700

    Header Component

--- view/alf/src/App.js
+++ view/alf/src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 import HomeContent from './components/HomeContent';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Lessons from './data/lessons';
 
 class App extends Component {
-  levels = Lessons.reduce(function (accumulator, currentValue) {
-    if (accumulator.indexOf(currentValue.level) === -1) {
-      accumulator.push(currentValue.level);
-    }
-    return accumulator;
-  }, []);
 
   render() {
     return (
       <div className="App">
-        <Header levels={this.levels}/>
+        <Header/>
         <HomeContent/>
         <Footer/>
       </div>
--- view/alf/src/components/Header.js
+++ view/alf/src/components/Header.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
+import Lessons from "../data/lessons";
 
 class Header extends Component {
-  render() {
-    const {levels} = this.props;
+  // need sort by levels after reduce
+  levels = Lessons.reduce(function (accumulator, currentValue) {
+    let lesson = {
+      title: currentValue.title,
+      url: currentValue.url,
+      id: currentValue.id
+    };
+    if (accumulator[currentValue.level] === undefined) {
+      accumulator[currentValue.level] = {
+        title: 'Level ' + currentValue.level,
+        lessons: []
+      };
+    }
+    accumulator[currentValue.level].lessons.push(lesson);
+    return accumulator;
+  }, []);
 
-    let levelDropDown = levels.map((level) => {
+  render() {
+    let levelDropDown = Object.keys(this.levels).map((key) => {
+      let level = this.levels[key];
       return (
-        <li className="nav-item dropdown">
+        <li key={key} className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
              aria-haspopup="true" aria-expanded="false">
-            Level {level}
+            {level.title}
           </a>
           <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">À l’hôtel</a>
-            <a className="dropdown-item" href="#">Au restaurant</a>
+            {level.lessons.map((lesson) => {
+              return (
+                <a key={lesson.id} className="dropdown-item" href={lesson.url}>{lesson.title}</a>
+              );
+            })}
           </div>
         </li>
       );
@@ -32,55 +52,18 @@ class Header extends Component {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
-                {/* <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Level A1
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">À l’hôtel</a>
-                    <a className="dropdown-item" href="#">Au restaurant</a>
-                  </div>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Level A2
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Bise à la française</a>
-                    <a className="dropdown-item" href="#">Comment utiliser les toilettes</a>
-                  </div>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Level B1
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Chapeaux en France</a>
-                    <a className="dropdown-item" href="#">Chefs cuisiniers célèbres</a>
-                  </div>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Level B2
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Bruxelles</a>
-                    <a className="dropdown-item" href="#">Cafetière à piston – comment l’utiliser ?</a>
-                  </div>
-                </li> */}
                 {levelDropDown}
               </ul>
               <ul className="navbar-nav ml-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="#"><strong>Sign Up</strong></a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#"><strong>Sign Up</strong></a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#"><strong>Login</strong></a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#"><strong>Login</strong></a>
                 </li>
               </ul>
             </div>
           </nav>
-
         </div>
       </div>
     );
