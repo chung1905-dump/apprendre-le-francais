cb9e51d046f517a3c39c4ede4d7059e378047bb1
chung <chung1905@gmail.com>
2018-11-04 18:24:03 +0700

    Server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(express.json());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+});
+
+module.exports = app;
