14361f63993e8e7b60fcb47717ca9d448d690fe8
nkhanhng97@gmail.com <khanhmatden123@gmail.com>
2018-11-29 23:47:03 +0700

    upload page

--- view/src/components/User/Upload.js
+++ view/src/components/User/Upload.js
@@ -1,10 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Upload extends Component {
   render() {
     return (
       <div className="container">
-        <span>Upload form go here</span>
+        <div>
+          <h2>Upload an audio</h2>
+        </div>
+        <div className="row justify-content-md-center">
+          <form>
+            <div className="form-group">
+              <label for="formGroupExampleInput">Title:</label>
+              <input type="text" class="form-control" name="title" placeholder="Title" />
+            </div>
+            <div className="form-group">
+              <label for="formGroupExampleInput2">Level</label>
+              <input type="text" className="form-control" name="level" placeholder="Level" />
+            </div>
+            <label>Your audio (mp3 only) :</label>
+            <div className="custom-file">
+              <input type="file" className="custom-file-input" id="customFile" />
+              <label className="custom-file-label">Choose file</label>
+            </div>
+            <label>Script :</label>
+            <div className="form-group">
+              <textarea className="form-control" id="Script" rows="3" placeholder="text here..."></textarea>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
