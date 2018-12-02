1c0832b5e6b1d5de74e54546547cc5f2d6c328d8
HOANG BOE <nguyenhoang11197@gmail.com>
2018-12-02 22:43:06 +0700

    model lesson

---
+++ server/model/lesson.js
@@ -0,0 +1,20 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var lessonSchema = new Schema({
+  title: {type: String, required: true},
+  script: {type: String, required: true},
+  user:{
+    type: Schema.Types.ObjectId,
+    ref: 'Users'
+  }
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Lesson = mongoose.model('Lesson', lessonSchema);
+
+// make this available to our users in our Node applications
+module.exports = Lesson;
